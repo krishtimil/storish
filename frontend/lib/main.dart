d58ea2e7082584d35d3cfb3b534f10c320b55941
krishtimil <30183049+krishtimil@users.noreply.github.com>
2023-01-29 02:12:23 +0545

    feat:Added transaction model

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import './screens/home_screen/home_screen.dart';
 import 'package:storish/utils/constant.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  Future<void> getData() async {
+    final response = await http
+        .get(Uri.parse('http://10.0.2.2:8000/api'), headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
+    print(response.body);
+  }
 
   // This widget is the root of your application.
   @override
